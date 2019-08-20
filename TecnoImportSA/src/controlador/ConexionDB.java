c112898d4e0fa14a76b454e7051bc5b50c102394
Jordy Villao <javillao@espol.edu.ec>
2019-08-19 21:18:34 -0500

    Bodega view

--- TecnoImportSA/src/controlador/ConexionDB.java
+++ TecnoImportSA/src/controlador/ConexionDB.java
@@ -123,4 +123,6 @@ public class ConexionDB {
     public Connection getConnection(){
         return instance.connection;
     }
+    
+    
 }
--- TecnoImportSA/src/vista/FormEntregaView.java
+++ TecnoImportSA/src/vista/FormEntregaView.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vista.basescreen;
+package vista;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
